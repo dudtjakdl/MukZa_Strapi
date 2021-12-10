8dfee4a1883cc47d807f7a54e8019d098236fe3c
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2021-12-10 19:18:28 +0900

    strapi start

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cbcf8bc838c806de4438128296c02beb'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '68caf775-38aa-4d67-a0f7-926e66872463'
+};
