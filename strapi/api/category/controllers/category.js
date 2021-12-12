0f058d2e22f8b45412420fd5ea91fdff345a1994
Yeongseo Kim <dudtjakdl@gmail.com>
2021-12-12 14:45:57 +0900

    mukit db 추가, category 컨트롤러 수정

--- strapi/api/category/controllers/category.js
+++ strapi/api/category/controllers/category.js
@@ -1,4 +1,4 @@
-const { sanitizeEntity } = require("strapi-utils");
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
@@ -11,6 +11,30 @@ module.exports = {
     const { name } = ctx.params;
 
     const entity = await strapi.services.category.delete({ name });
+    return sanitizeEntity(entity, { model: strapi.models.category });
+  },
+  async findOne(ctx) {
+    const { name } = ctx.params;
+
+    const entity = await strapi.services.category.findOne({ name });
+    return sanitizeEntity(entity, { model: strapi.models.category });
+  },
+  async update(ctx) {
+    const { name } = ctx.params;
+
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.category.update({ name }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.category.update(
+        { name },
+        ctx.request.body
+      );
+    }
+
     return sanitizeEntity(entity, { model: strapi.models.category });
   },
 };
